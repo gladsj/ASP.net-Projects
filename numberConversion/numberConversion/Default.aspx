7c1ad1b4d6fe8c0ad1a07288f6ed0c7eae8a5588
gladsj <104738537+gladsj@users.noreply.github.com>
2023-02-21 04:01:53 -0600

    Add files via upload

---
+++ numberConversion/numberConversion/Default.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="numberConversion.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<title></title>
+     <link href="MyStyle.css" rel="stylesheet" />
+     <style type="text/css">
+          .auto-style1 {
+               width: 600px;
+               height: 600px;
+               margin-left: 0px;
+               margin-bottom: 0px;
+          }
+          .auto-style2 {
+               width: 600px;
+               height: 145px;
+          }
+     </style>
+</head>
+<body id="Frame">
+     
+<form id="form1" runat="server" class="auto-style1">
+          <img alt="" class="auto-style2" src="Images/numberConversion%20(1).jpg" id="imgBanner" /><br />
+     <br />
+     <asp:Label ID="lblInputNum" runat="server" CssClass="Title" Font-Size="Large" Text="Enter Number"></asp:Label>
+     <br />
+<asp:TextBox ID="TextBox1" runat="server" Width="253px" OnTextChanged="TextBox1_TextChanged" CssClass="textBox" Height="30px"></asp:TextBox>
+
+     <br />
+     <br />
+     <asp:Label ID="lblInputddl" runat="server" CssClass="Title" Font-Size="Large" Text="From Base"></asp:Label>
+     <br />
+
+     <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" Width="260px" CssClass="textBox" Height="32px">
+<asp:ListItem>Select</asp:ListItem>
+<asp:ListItem>Decimal</asp:ListItem>
+<asp:ListItem>Binary</asp:ListItem>
+<asp:ListItem>HexaDecimal</asp:ListItem>
+<asp:ListItem>Octal</asp:ListItem>
+</asp:DropDownList>
+     <br />
+     <br />
+     <asp:Label ID="lblOutputddl" runat="server" CssClass="Title" Font-Size="Large" Text="To Base"></asp:Label>
+     <br />
+
+<asp:DropDownList ID="DropDownList2" runat="server" Height="32px" Width="260px" CssClass="textBox">
+</asp:DropDownList>
+
+     <br />
+     <br />
+<asp:Button ID="Button1" runat="server" Text="Convert" Width="107px" OnClick="Button1_Click" CssClass="Button" />
+     &nbsp;&nbsp;&nbsp;
+     <asp:Button ID="Button2" runat="server" Text="Reset" Width="107px" OnClick="Button2_Click" CssClass="Labels" />
+
+     <br />
+     <br />
+     <asp:Label ID="Label2" runat="server" Font-Bold="True" CssClass="Solution">Your Answer</asp:Label>
+</form>
+     </body>
+</html>
---
+++ numberConversion/numberConversion/Default.aspx.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace numberConversion
+{
+     public partial class Default : System.Web.UI.Page
+     {
+          protected void Page_Load(object sender, EventArgs e)
+          {
+
+          }
+
+          //fisrt drop down list change event  
+
+          protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+          {
+
+               //DropDownList2.Items.Clear();
+               string str = DropDownList1.SelectedItem.Value;
+               if (str == "Binary")
+               {
+                    DropDownList2.Items.Add(new ListItem("Select"));
+                    DropDownList2.Items.Add(new ListItem("HexaDecimal"));
+                    DropDownList2.Items.Add(new ListItem("Decimal"));
+                    DropDownList2.Items.Add(new ListItem("Octal"));
+               }
+               else if (str == "Decimal")
+               {
+                    DropDownList2.Items.Add(new ListItem("Select"));
+                    DropDownList2.Items.Add(new ListItem("HexaDecimal"));
+                    DropDownList2.Items.Add(new ListItem("Binary"));
+                    DropDownList2.Items.Add(new ListItem("Octal"));
+               }
+               else if (str == "HexaDcimal")
+               {
+                    DropDownList2.Items.Add(new ListItem("Select"));
+                    DropDownList2.Items.Add(new ListItem("Binary"));
+                    DropDownList2.Items.Add(new ListItem("Decimal"));
+                    DropDownList2.Items.Add(new ListItem("Octal"));
+               }
+               else if (str == "Octal")
+               {
+                    DropDownList2.Items.Add(new ListItem("Select"));
+                    DropDownList2.Items.Add(new ListItem("HexaDecimal"));
+                    DropDownList2.Items.Add(new ListItem("Decimal"));
+                    DropDownList2.Items.Add(new ListItem("Binary"));
+               }
+          }
+
+          protected void Button1_Click(object sender, EventArgs e)//this is for the calculate button
+          {
+               int num = Convert.ToInt16(TextBox1.Text);
+               string to = DropDownList2.SelectedItem.Value;
+               if (DropDownList1.SelectedValue == "Decimal")
+               {
+                    if (to == "Binary")
+                    {
+                         Label2.Text = deciTobin(num);
+                    }
+                    else if (to == "HexaDecimal")
+                    {
+                         string myhex = Convert.ToString(Convert.ToInt32(deciTobin(num), 2), 16);
+                         Label2.Text = myhex;
+                    }
+                    else if (to == "Octal")
+                    {
+                         Label2.Text = deciToOct(num);
+                    }
+
+                   
+               }
+          }
+          public string deciTobin(int no)
+          {
+               int quot;
+               string rem = "";
+               while (no >= 1)
+               {
+                    quot = no / 2;
+                    rem += (no % 2).ToString();
+                    no = quot;
+               }
+               string bin = "";
+               for (int i = rem.Length - 1; i >= 0; i--)
+               {
+                    bin = bin + rem[i];
+               }
+               return bin;
+          }
+          public string deciToOct(int num)
+          {
+               int[] octalNumber = new int[100];
+               int quotient, i = 1, j;
+               string ans = "";
+               quotient = num;
+               while (quotient != 0)
+               {
+                    octalNumber[i++] = quotient % 8;
+                    quotient = quotient / 8;
+               }
+               Console.Write("Equivalent Octal Number is ");
+               for (j = i - 1; j > 0; j--)
+                    ans += octalNumber[j].ToString();
+               return ans;
+          }
+
+          protected void Button2_Click(object sender, EventArgs e)//this is for the reset button
+          {
+               TextBox1.Text = "";
+               DropDownList1.ClearSelection();
+               DropDownList1.SelectedValue = "Select";
+               DropDownList2.ClearSelection();
+               DropDownList2.SelectedValue = "Select";
+               Label2.Text = "";
+          }
+
+          protected void TextBox1_TextChanged(object sender, EventArgs e)
+          {
+
+          }
+     }
+}
